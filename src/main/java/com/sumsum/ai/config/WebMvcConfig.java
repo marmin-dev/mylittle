1c5037233cac2971850b6394c103c00eb2cd1d4e
marmin-dev <marmin.dev@gmail.com>
2023-05-01 18:52:13 +0900

    add cors code

---
+++ src/main/java/com/sumsum/ai/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.sumsum.ai.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 모든 경로에 대해
+        long MAX_AGE_SPECS = 3600;
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SPECS);
+    }
+}
